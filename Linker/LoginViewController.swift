2148c45518f1bd958a29d8efb61719b974feb10c
IdleApps <IdleAppsInc10@gmail.com>
2016-12-11 16:26:29 +0000

    Testing 3

--- Linker/LoginViewController.swift
+++ Linker/LoginViewController.swift
@@ -21,7 +21,7 @@ class LoginViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        print("TESTING GITHUB!")
+        print("Hello")
     }
     
     
